50745227ca85ce9997042904453eb9c3b9f27c72
Lehman Black <WileESpaghetti@users.noreply.github.com>
2020-08-05 20:35:48 -0500

    switch to first_name and last_name instead of name column for users

---
+++ database/migrations/2020_08_06_010850_add_first_and_last_name_to_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFirstAndLastNameToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->default('');
+            $table->string('last_name')->default('');
+        });
+
+        // TODO copy data from users.name to users.first_name and users.last_name
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->default('');
+        });
+
+         // TODO move data from users.first_name and users.last_name to users.name
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+        });
+    }
+}
